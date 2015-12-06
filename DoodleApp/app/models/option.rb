13280d9ba1e1627476bf758c4453196e0068d70d
AndGrt <andreea.gurita95@yahoo.com>
2015-12-06 13:51:43 +0200

    app

--- DoodleApp/app/models/option.rb
+++
@@ -1,5 +0,0 @@
-class Option < ActiveRecord::Base
-  has_many :actual_choices
-  has_many :participants, :through => :actual_choices
-end
-
--- DoodleApp/app/models/participant.rb
+++
@@ -1,5 +0,0 @@
-class Participant < ActiveRecord::Base
-  has_many :actual_choices
-  has_many :options, :through => :actual_choices
-end
-
